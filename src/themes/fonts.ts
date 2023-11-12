f4cb88297db7203e6766a7354cfc49fb6ca7ec57
Psalishol samuel <psalishol80@gmail.com>
2023-11-12 15:30:41 +0100

    add app font

---
+++ src/themes/fonts.ts
@@ -0,0 +1,45 @@
+const fonts = {
+  // Inter font definition
+  InterBlack: 'Inter-Black',
+  InterBold: 'Inter-Bold',
+  InterExtrabold: 'Inter-ExtraBold',
+  InterExtraLight: 'Inter-ExtraLight',
+  InterLight: 'Inter-Light',
+  InterMedium: 'Inter-Medium',
+  InterRegular: 'Inter-Regular',
+  InterSemiBold: 'Inter-SemiBold',
+  InterThin: 'Inter-Thin',
+
+  //Poppins definition
+  PoppinsBlack: 'Poppins-Black',
+  PoppinsBlackItalic: 'Poppins-BlackItalic',
+  PoppinsBold: 'Poppins-Bold',
+  PoppinsBoldItalic: 'Poppins-BoldItalic',
+  PoppinsExtraBold: 'Poppins-ExtraBold',
+  PoppinsExtraBoldItalic: 'Poppins-ExtraBoldItalic',
+  PoppinsExtraLight: 'Poppins-ExtraLight',
+  PoppinsExtraLightItalic: 'Poppins-ExtraLightItalic',
+  PoppinsItalic: 'Poppins-Italic',
+  PoppinsMedium: 'Poppins-Medium',
+  PoppinsMediumItalic: 'Poppins-MediumItalic',
+  PoppinsRegular: 'Poppins-Regular',
+  PoppinsSemiBold: 'Poppins-SemiBold',
+  PoppinsSemiBoldItalic: 'Poppins-SemiBoldItalic',
+  PoppinsThin: 'Poppins-Thin',
+  PoppinsThinItalic: 'Poppins-ThinItalic',
+
+  // Roboto fonts definitions
+  RobotoBold: 'Roboto-Bold',
+  RobotoBlack: 'Roboto-Black',
+  RobotoBlackItalic: 'Roboto-BlackItalic',
+  RobotoItalic: 'Roboto-Italic',
+  RobotoLightItalic: 'Roboto-LightItalic',
+  RobotoLight: 'Roboto-Light',
+  RobotoMedium: 'Roboto-Medium',
+  RobotoMediumItalic: 'Roboto-MediumItalic',
+  RobotoRegular: 'Roboto-Regular',
+  RobotoThin: 'Roboto-Thin',
+  RobotoThinItalic: 'Roboto-ThinItalic',
+};
+
+export {fonts};
