5410ecc9d488d638e1590d026b2f975c88c7078a
Psalishol samuel <psalishol80@gmail.com>
2023-11-12 15:39:51 +0100

    update: initialize gesture handler before app entry

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-/**
- * @format
- */
+// This is included for early initialisation of gesture handler.
+// Gesture handler uses native API, Hence it needs to initialize before the app finishes loading to start
+// recieving touch events as soon as possible
+import 'react-native-gesture-handler';
 
 import {AppRegistry} from 'react-native';
 import App from './App';
